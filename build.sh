acef7c1937994dfa27125e56fa047b4cde11d2c5
jblues <jasper@appsquick.ly>
2015-11-21 13:17:54 +0800

    Share schemes. Fix build script.

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+
+#Fail immediately if a task fails
+set -e
+set -o pipefail
+
 xcodebuild -project XcodeEditor.xcodeproj -scheme XcodeEditor clean test | xcpretty -c --report junit
-groovy http://frankencover.it/with --source-dir Source
+groovy http://frankencover.it/with --source-dir Source -r 76
