6410b7a92306aa67a661c82369337c5081101c5b
jblues <jasper@appsquick.ly>
2015-11-22 17:47:00 +0800

    Restore framework target.

--- Source/XcodeEditor.h
+++ Source/XcodeEditor.h
@@ -9,6 +9,16 @@
 //
 ////////////////////////////////////////////////////////////////////////////////
 
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for XcodeEditor.
+FOUNDATION_EXPORT double XcodeEditorVersionNumber;
+
+//! Project version string for XcodeEditor.
+FOUNDATION_EXPORT const unsigned char XcodeEditorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XcodeEditor/PublicHeader.h>
+
 
 #import "XCAbstractDefinition.h"
 #import "XCGroup.h"
